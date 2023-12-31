a3bd37325bc038da9401de787e4009483d82846f
João Afonso <joaoafonso386@gmail.com>
2023-12-31 03:52:52 +0000

    Remove ts-node-dev as it is trash, only tsc compiler for now and looking for new options

--- dist/cli/test/cli.test.js
+++ dist/cli/test/cli.test.js
@@ -1,1 +1,15 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const DEF_ITEMS = {
+    nome: "Flash",
+    power: "Speed",
+    id: 1
+};
+describe("Manipulate Heroes", () => {
+    it("should find a hero", () => {
+    });
+    it("Should register a hero", () => {
+        const expected = DEF_ITEMS;
+        // ok(null, expected)
+    });
+});
---
+++ src/cli/test/cli.test.ts
@@ -0,0 +1,19 @@
+import { deepEqual, ok } from "assert";
+
+
+const DEF_ITEMS = {
+    nome: "Flash",
+    power: "Speed",
+    id: 1
+}
+
+describe("Manipulate Heroes", () => {
+    it("should find a hero", () => {
+        
+    })
+    it("Should register a hero", () => {
+        const expected = DEF_ITEMS
+
+        // ok(null, expected)
+    })
+}) 
