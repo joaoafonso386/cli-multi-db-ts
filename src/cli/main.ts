5fa37827b7c42b1ff067a39cc03fbcff081e67c7
João Afonso <joaoafonso386@gmail.com>
2024-01-06 02:28:45 +0000

    Add update flag to cli

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,24 +1,25 @@
 import { Command } from "commander";
 import { HeroesDB } from "./db/db";
-import { registerHeroValidator } from "./validators/register-hero-validator";
+import { nameAndPowerValidator } from "./validators/validators";
 
 const main = async () => {
    const cli = new Command()
    cli
       .version('v1')
       .option('-l, --list', 'List all heros')
+      .option('-rm, --remove [value]', 'Delete a hero by id or pass nothing to remove all heroes')
+      .option('-up, --update [value]', 'Update a hero by id - use -n and -p to add new values')
       .option('-r, --register', 'Define that you will be registering a hero')
-      .option('-rm, --remove [value]', 'Delete a hero by id')
-      .option('-n, --name [value]', 'Hero name')
-      .option('-p, --power [value]', 'Heros Power')
+      .option('-n, --name [value]', 'Name of the hero to register')
+      .option('-p, --power [value]', 'Power of the hero to register')
       .parse(process.argv)
 
-   const { name, power, id, register, list, remove } = cli.opts()
+   const { name, power, id, register, list, remove, update } = cli.opts()
    const db = new HeroesDB()
 
    try {
       if(register) {
-         const { validName, validPower } = registerHeroValidator(name, power)
+         const { validName, validPower } = nameAndPowerValidator(name, power)
          const res = await db.createHero({ name: validName, power: validPower, id })
          if(!res) {
             console.error('There was an error registering the Hero')
@@ -37,13 +38,17 @@ const main = async () => {
       if(remove) {
          const id = parseInt(remove)
          const deletedHero = await db.removeHero(id)
-         console.log(`It is ${deletedHero} that your hero has been removed!`)
+         console.log(`It is ${deletedHero} that your hero/heroes have been deleted!`)
          return
       }
 
-      console.log(cli.opts())
-
-
+      if(update) {
+         const id = parseInt(update)
+         const { validName, validPower } = nameAndPowerValidator(name, power)
+         const updatedHero = await db.updateHero(id, { name: validName, power: validPower})
+         console.log(`It is ${updatedHero} that your hero has been updated!`)
+      } 
+      
    } catch(e) {
       console.log(e)
    }
--- src/cli/validators/validators.ts
+++ src/cli/validators/validators.ts
@@ -1,6 +1,6 @@
 import { CliArg } from "cli/types/types"
 
-export const registerHeroValidator = (cliName: CliArg, cliPower: CliArg) => {
+export const nameAndPowerValidator = (cliName: CliArg, cliPower: CliArg) => {
     if(!cliName || !cliPower) throw new Error("name '-n' and power '-p' are both required arguments")
     const validName = typeof cliName === 'boolean' ? 'Dummy Hero Name' : cliName
     const validPower = typeof cliPower === 'boolean' ? 'Dummy Hero Power' : cliPower
