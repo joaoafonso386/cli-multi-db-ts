ede3be21849a0def230cb4cf0cb7086e13b2892f
João Afonso <joaoafonso386@gmail.com>
2024-01-06 01:20:37 +0000

    Add register flag

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -6,20 +6,24 @@ const main = async () => {
    cli
       .version('v1')
       .option('-n, --name [value]', 'Hero name')
+      .option('-r, --register [value]', 'Define that you will be registering a hero')
       .option('-p, --power [value]', 'Heros Power')
       .parse(process.argv)
 
-   const { name, power, id } = cli.opts()
+   const { name, power, id, register } = cli.opts()
    const db = new HeroesDB()
 
    try {
-      if(!name || !power) throw new Error("name '-n' and power '-p' are both required arguments")
-      const res = await db.createHero({ name, power, id })
-      if(!res) {
-         console.error('There was an error registering the Hero')
-         return
+      if(register) {
+         console.log(cli.opts())
+         if(!name || !power) throw new Error("name '-n' and power '-p' are both required arguments")
+         const res = await db.createHero({ name, power, id })
+         if(!res) {
+            console.error('There was an error registering the Hero')
+            return
+         }
+         console.log('Hero successfully registered')
       }
-      console.log('Hero successfully registered')
    } catch(e) {
       console.log(e)
    }
